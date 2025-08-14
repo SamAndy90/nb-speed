efcb66b70e6468792def90d814c5668c08ad6544
Andrii Stadnyk <stadnyk.andy@gmail.com>
2025-08-14 20:56:25 +0300

    test 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,14 +92,14 @@ const heroOptions: HeroOption[] = [
 export default async function Home() {
     return (
         <main className="flex min-h-screen w-full flex-col items-center justify-between overflow-x-hidden">
-            <GradientBackground variant="section-2">
+            {/* <GradientBackground variant="section-2">
                 <Hero options={heroOptions} />
                 <Suspense fallback={<Loader />}>
                     <BackedByScience />
                 </Suspense>
-            </GradientBackground>
+            </GradientBackground> */}
             <Founder />
-            <CompetitionModal
+            {/* <CompetitionModal
                 headingText="Buy 2 Nutriburst Minion Products for a chance to"
                 actionText="WIN"
                 prizeDescription="A family holiday for 4 to Minion World in Orlando, Florida"
@@ -110,23 +110,23 @@ export default async function Home() {
                 imageAlt="Minions Competition"
                 mobileImageSrc="https://cdn.shopify.com/s/files/1/0072/6325/6685/files/Minions_Mega_Multi_PDP_01.png?v=1751879554"
                 sessionStorageKey="minions-competition-modal-shown"
-            />
+            /> */}
             <Suspense fallback={null}>
                 <ReferrerToast />
             </Suspense>
             <GummiesRedefined />
-            <DiscoverOurCollections />
+            {/* <DiscoverOurCollections /> */}
             <SectionSpacer hasBackground={true} />
-            <GradientBackground variant="section-3">
+            {/* <GradientBackground variant="section-3">
                 <LovedBy />
-            </GradientBackground>
+            </GradientBackground> */}
             <SectionSpacer hasBackground={true} />
             <GotQuestions />
             <SectionSpacer hasBackground={true} />
             <FindUsIn />
             <SectionSpacer hasBackground={true} />
-            <TrustedByExperts />
-            <InstagramFeeds />
+            {/* <TrustedByExperts /> */}
+            {/* <InstagramFeeds /> */}
             <GradientBackground variant="section-4">
                 <Suspense fallback={<Loader />}>
                     <BlogPosts />
